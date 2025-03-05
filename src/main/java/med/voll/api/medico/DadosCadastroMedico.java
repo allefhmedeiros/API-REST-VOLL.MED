dbdc1948b5af8d5ad24e5162db77fd0b791bd90a
allefhmedeiros <allefhmedeiros@gmail.com>
2025-03-05 20:16:39 -0300

    Implementado o repository de médico, ajustada a documentação do projeto e implementada as migration do banco de dados, V1 implementação a tabela usuarios e V2 atualização da tabela inserindo a coluna de telefone.

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,27 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid; //BeanValidation
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank                               //NÃO ACEITA STRING VAZIA
+        String nome,
+        @NotBlank                               //NÃO ACEITA STRING VAZIA
+        @Email                                  //VALIDA A ESTRUTURA DA STRING, PRECISA SEGUIR AS REGRAS DE COMPOSIÇÃO DE E-MAIL
+        String email,
+        @NotBlank                               //NÃO ACEITA STRING VAZIA
+        String telefone,
+        @NotBlank                               //NÃO ACEITA STRING VAZIA
+        @Pattern(regexp = "\\d{4,6}")           //VALIDA O FORMATO DO VALOR INSERIDO, NESSE CASO SÃO NÚMEROS DE 4 A 6 DIGITOS
+        String crm,
+        @NotNull                                //NÃO ACEITA VALOR VAZIO, PREENCHIMENTO OBRIGATÓRIO
+        Especialidade especialidade,
+        @NotNull                                //NÃO ACEITA VALOR VAZIO, PREENCHIMENTO OBRIGATÓRIO
+        @Valid                                  //INFORMA QUE DENTRO DO OBJETO ENDEREÇO TAMBÉM HÁ CAMPOS QUE DEVEM SER VALIDADOS.
+        DadosEndereco endereco) {
 
 }
